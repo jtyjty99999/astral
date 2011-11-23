83b183b2ab29c08d71ea4738c625e711d42e3ee7
Adams <jtyjty99999@126.com>
2011-11-24 01:07:18 +0800

    set up my js factory

--- serendipity.js
+++ serendipity.js
@@ -1,3 +1,17 @@
 /**
  * @author Administrator
  */
+function getPos(obj)
+		{
+			var left = 0;
+			var top = 0;
+
+			while(obj.offsetParent)
+			{
+				left+=obj.offsetLeft;
+				top+=obj.offsetTop;
+
+				obj=obj.offsetParent;
+			}
+			return {x:left,y:top};
+		}
