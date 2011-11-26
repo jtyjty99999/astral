ec6a29f3ed8f52b279c8601de0a1c9c15b79975f
Adams <jtyjty99999@126.com>
2011-11-26 23:40:25 +0800

    Update serendipity.js

--- serendipity.js
+++ serendipity.js
@@ -1,6 +1,10 @@
 /**
  * @author Administrator
  */
+ 
+/**
+get an element the absolute position of the window 
+ */ 
 function getPos(obj)
 		{
 			var left = 0;
@@ -15,3 +19,26 @@ function getPos(obj)
 			}
 			return {x:left,y:top};
 		}
+		
+/**
+compatibility type of height
+ */ 
+var UI = {  
+    wholeHeight: function() {  
+        return document.body.scrollHeight || document.documentElement.scrollHeight  
+    },  
+    windowHeight: function () {  
+        var a = document.documentElement;  
+        return self.innerHeight || a && a.clientHeight || document.body.clientHeight  
+    },  
+    scrollY: function (a) {  
+        var b = document.documentElement;  
+        if (a) {  
+            var c = a.parentNode,  
+                e = a.scrollTop || 0;  
+            if (a == b) e = UI.scrollY();  
+            return c ? e + UI.scrollY(c) : e  
+        }  
+        return self.pageYOffset || b && b.scrollTop || document.body.scrollTop  
+    }  
+};  
