fd6613c02724f39c00e7481eb9e46d7c46ba70d8
Adams <jtyjty99999@126.com>
2011-12-27 11:53:45 +0800

    Update serendipity.js

--- serendipity.js
+++ serendipity.js
@@ -15,6 +15,21 @@ function $id(id){
 function $tag(tag){
 	return document.getElementsByTagName(tag)
 }
+var get = {
+	byId: function(id) {
+		return document.getElementById(id)
+	},
+	byClass: function(sClass, oParent) {
+		var aClass = [];
+		var reClass = new RegExp("(^| )" + sClass + "( |$)");
+		var aElem = this.byTagName("*", oParent);
+		for (var i = 0; i < aElem.length; i++) reClass.test(aElem[i].className) && aClass.push(aElem[i]);
+		return aClass
+	},
+	byTagName: function(elem, obj) {
+		return (obj || document).getElementsByTagName(elem)
+	}
+};
 /*-------------------------- +
  /delete current element node
  +-------------------------- */
