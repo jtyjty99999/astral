81f5ed56d13d9b7a6c12d21b52e720791781670f
Adams <jtyjty99999@126.com>
2011-12-30 19:43:37 +0800

    Update serendipity.js

--- serendipity.js
+++ serendipity.js
@@ -170,9 +170,79 @@ function getCss(elem, css){
 		return elem.style[css];
 	}
 }
+
+/**
+setOpacity
+ */ 
+ 
+function setOpacity(elem,opacity){
+	elem.filters ? elem.style.filter = 'alpha(opacity=' + opacity + ')':elem.style.opacity = opacity / 100;}
+/**
+set opacity fadeIn fadeOut effect @Mr.Think
+ */ 	
+	//底层共用
+    var iBase = {
+        Id: function(name){
+            return document.getElementById(name);
+        },
+		//设置元素透明度,透明度值按IE规则计,即0~100
+        SetOpacity: function(ev, v){
+            ev.filters ? ev.style.filter = 'alpha(opacity=' + v + ')' : ev.style.opacity = v / 100;
+        }
+    }
+	//淡入效果(含淡入到指定透明度)
+	function fadeIn(elem, speed, opacity){
+		/*
+		 * 参数说明
+		 * elem==>需要淡入的元素
+		 * speed==>淡入速度,正整数(可选)
+		 * opacity==>淡入到指定的透明度,0~100(可选)
+		 */
+	    speed = speed || 20;
+	    opacity = opacity || 100;
+		//显示元素,并将元素值为0透明度(不可见)
+	    elem.style.display = 'block';
+	    iBase.SetOpacity(elem, 0);
+		//初始化透明度变化值为0
+	    var val = 0;
+		//循环将透明值以5递增,即淡入效果
+	    (function(){
+	        iBase.SetOpacity(elem, val);
+	        val += 5;
+	        if (val <= opacity) {
+	            setTimeout(arguments.callee, speed)
+	        }
+	    })();
+	}
+	
+	//淡出效果(含淡出到指定透明度)
+	function fadeOut(elem, speed, opacity){
+		/*
+		 * 参数说明
+		 * elem==>需要淡入的元素
+		 * speed==>淡入速度,正整数(可选)
+		 * opacity==>淡入到指定的透明度,0~100(可选)
+		 */
+	    speed = speed || 20;
+	    opacity = opacity || 0;
+	    //初始化透明度变化值为0
+	    var val = 100;
+		//循环将透明值以5递减,即淡出效果
+	    (function(){
+	        iBase.SetOpacity(elem, val);
+	        val -= 5;
+	        if (val >= opacity) {
+	            setTimeout(arguments.callee, speed);
+	        }else if (val < 0) {
+				//元素透明度为0后隐藏元素
+	            elem.style.display = 'none';
+	        }
+	    })();
+
 /**
 get an element the absolute position of the window 
- */ 
+ */
+ 
 function getPos(obj)
 		{
 			var left = 0;
