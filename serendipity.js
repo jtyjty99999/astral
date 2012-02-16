70aca6462a38d4e2bded7ac3f32334ff5144affb
Adams <jtyjty99999@126.com>
2012-02-17 00:07:03 +0800

    Update serendipity.js

--- serendipity.js
+++ serendipity.js
@@ -20,15 +20,34 @@
 			alert('这货不是ie')
 		}
 	}
-	$.getOS = function () {
-		var a = navigator.userAgent.toLowerCase();
-		var b = /(firefox|netscape|opera).?[\/| ](.)\.([^;\)]+|[^\)]+\))$/.exec(a);
-		if (!b)
-			b = /(msie) (.)\.[^;]+;/.exec(a);
-		var _Br = b[1],
-		_Ver = b[2];
-		return (_Br + _Ver);
-	}
+	/*-------------------------- +
+	Browser navigation from QWARP
+	+-------------------------- */
+	$. Browser = (function() {
+var na = window.navigator,
+ua = na.userAgent.toLowerCase(),
+browserTester = /(msie|webkit|gecko|presto|opera|safari|firefox|chrome|maxthon|android|ipad|iphone|webos|hpwos)[ \/os]*([\d_.]+)/ig,
+Browser = {
+platform: na.platform
+};
+ua.replace(browserTester, function(a, b, c) {
+var bLower = b.toLowerCase();
+if (!Browser[bLower]) {
+Browser[bLower] = c;
+}
+});
+if (Browser.opera) { //Opera9.8后版本号位置变化
+ua.replace(/opera.*version\/([\d.]+)/, function(a, b) {
+Browser.opera = b;
+});
+}
+if (Browser.msie) {
+Browser.ie = Browser.msie;
+var v = parseInt(Browser.msie, 10);
+Browser['ie' + v] = true;
+}
+return Browser;
+}());
 	//Dom加载完成事件
 	$.ready = function (loadEvent) {
 		var init = function () {
