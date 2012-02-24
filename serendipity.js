acb8ae11281aa6546937d285e7d1cc37ccfb276d
Adams <jtyjty99999@126.com>
2012-02-24 11:43:32 +0800

    Update serendipity.js

--- serendipity.js
+++ serendipity.js
@@ -186,6 +186,23 @@
 		return Object.prototype.toString.call(object).match(/^\[object\s(.*)\]$/)[1];
 	};
 	/*-------------------------- +
+	concat 2 array and remove duplication
+	+-------------------------- */	
+	function concatAnd(arr1, arr2) {
+		for (var i = 0; i < arr1.length; i++) {
+			for (var j = 0; j < arr2.length; j++) {
+				if (arr1[i] === arr2[j]) {
+					arr1.splice(i, 1);
+				}
+			}
+		}
+		//alert(arr1.length)
+		for (var i = 0; i < arr2.length; i++) {
+			arr1.push(arr2[i]);
+		}
+		return arr1;
+	}	
+	/*-------------------------- +
 	GetIframe
 	+-------------------------- */
 	$.getIframe = function getIFrameDOM(id) {
