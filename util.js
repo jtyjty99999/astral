9f03f2fd3deb48ccb97d8a46ca6913040895d4f3
jty <jtyjty99999@126.com>
2013-08-23 21:12:14 +0800

    updated

--- util.js
+++ util.js
@@ -29,9 +29,31 @@ define(function () {
 	Util.getType =function(object) {
 		return Object.prototype.toString.call(object).match(/^\[object\s(.*)\]$/)[1];
 	};
-	Util.getType =function(object) {
-		return Object.prototype.toString.call(object).match(/^\[object\s(.*)\]$/)[1];
-	};
+/**
+	 * 揉杂，合并对象
+	 *
+	 * @param {"[object Object]"} base 揉杂的接受者
+	 * @param {"[object Object]"} supplier  提供者
+	 * @param {"[object Boolean]"} bOverwrite 存在相同key时,若不覆盖原对象，取true. 默认为false,覆盖原对象
+	 * @return {"[object Object]"} 揉杂结果  
+	 * @public
+mixin({a:111,b:222,c:333},{a:222})//{a: 222, b: 222, c: 333}
+mixin({a:111,b:222,c:333},{a:222},true)//{a: 111, b: 222, c: 333}
+**/
+	Util.mixin = function(base, supplier, bOverwrite) {
+		var base = base || {},
+		key,
+		bOverwrite = bOverwrite || false;
+		for (key in supplier) {
+			if (supplier.hasOwnProperty(key)) { //不像 for in operator, hasownproperty 不追踪prototype chain
+				if (typeof(base[key]) != 'undefined' && bOverwrite) {
+					continue;
+				}
+				base[key] = supplier[key];
+			}
+		}
+		return base;
+	}
 
 	
 	return Util
