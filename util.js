7b9e8fccfc5a2d5b67c07ceebd1ed9986eb4a724
jty <jtyjty99999@126.com>
2013-08-28 01:25:16 +0800

    add 	☺isEqual 2013.8.28 ☺isWindow 2013.8.28 ☺isEmpty 2013.8.28

--- util.js
+++ util.js
@@ -3,18 +3,19 @@
  *
  * 基础工具库,提供重要的几个方法
  */
- 
-  (function(name, definition) {
-    if(typeof define == 'function') {
-        define(definition);
-    } else if(typeof module != 'undefined' && module.exports) {
-        module.exports = definition;
-    } else {
-        window[name] = definition;
-    }
-})('Util',function () {
+
+(function (name, definition) {
+	if (typeof define == 'function') {
+		define(definition);
+	} else if (typeof module != 'undefined' && module.exports) {
+		module.exports = definition;
+	} else {
+		window[name] = definition;
+	}
+})('Util', function () {
 	var Util = {};
-	var objProto = Object.prototype,hasOwn = objProto.hasOwnProperty;
+	var objProto = Object.prototype,
+	hasOwn = objProto.hasOwnProperty;
 	/**
 	 * Returns internal [[Class]] property of an object
 	 *
@@ -36,21 +37,21 @@
 	 * getType(Element); // => "Constructor"
 	 *
 	 */
-	Util.getType =function(object) {
+	Util.getType = function (object) {
 		return objProto.toString.call(object).match(/^\[object\s(.*)\]$/)[1];
 	};
-/**
+	/**
 	 * 揉杂，合并对象
 	 *
 	 * @param {"[object Object]"} base 揉杂的接受者
 	 * @param {"[object Object]"} supplier  提供者
 	 * @param {"[object Boolean]"} bOverwrite 存在相同key时,若不覆盖原对象，取true. 默认为false,覆盖原对象
-	 * @return {"[object Object]"} 揉杂结果  
+	 * @return {"[object Object]"} 揉杂结果
 	 * @public
-mixin({a:111,b:222,c:333},{a:222})//{a: 222, b: 222, c: 333}
-mixin({a:111,b:222,c:333},{a:222},true)//{a: 111, b: 222, c: 333}
-**/
-	Util.mixin = function(base, supplier, bOverwrite) {
+	mixin({a:111,b:222,c:333},{a:222})//{a: 222, b: 222, c: 333}
+	mixin({a:111,b:222,c:333},{a:222},true)//{a: 111, b: 222, c: 333}
+	 **/
+	Util.mixin = function (base, supplier, bOverwrite) {
 		var base = base || {},
 		key,
 		bOverwrite = bOverwrite || false;
@@ -64,104 +65,122 @@ mixin({a:111,b:222,c:333},{a:222},true)//{a: 111, b: 222, c: 333}
 		}
 		return base;
 	}
-/**
+	/**
 	 * 遍历器
 	 *
 	 * @param {"[object Object]"} O 被遍历对象
 	 * @param {"[object Function]"} 回调函数
-	 * @return {"[object Object]"} 
+	 * @return {"[object Object]"}
 	 * @public
-mixin({a:111,b:222,c:333},{a:222})//{a: 222, b: 222, c: 333}
-mixin({a:111,b:222,c:333},{a:222},true)//{a: 111, b: 222, c: 333}
-**/	
-	
-	Util.each=function( O, callbackfn ) {
+	mixin({a:111,b:222,c:333},{a:222})//{a: 222, b: 222, c: 333}
+	mixin({a:111,b:222,c:333},{a:222},true)//{a: 111, b: 222, c: 333}
+	 **/
 
-            var T, k = 0, kValue, len;
+	Util.each = function (O, callbackfn) {
 
-            if ( O == null ) {  
-                throw new TypeError( 'this is null or not defined' );  
-            }
+		var T,
+		k = 0,
+		kValue,
+		len;
 
-            O = Object(O);
-            len = O.length >>> 0;
+		if (O == null) {
+			throw new TypeError('this is null or not defined');
+		}
 
-            if( getType(callbackfn) != 'Function' ) {
-                throw new TypeError( callbackfn + ' is not a function' );
-            }
+		O = Object(O);
+		len = O.length >>> 0;
 
-            if( getType(O) == 'Array' ) {
-                while(k < len) {
-                    if( hasOwn.call( O, k ) ) {
-                        kValue = O[k];
-                        if( callbackfn.call( kValue, k + 1, kValue, O ) === false ) break;
-                    }
-                    k ++;
-                }
-            } else {
-                for( k in O ) {
-                    if( hasOwn.call(O, k) ) {
-                        kValue = O[k];
-                        if( callbackfn.call( kValue, kValue, k, O ) === false ) break;
-                    }
-                }
-            }
-            return undefined;
-        }
+		if (getType(callbackfn) != 'Function') {
+			throw new TypeError(callbackfn + ' is not a function');
+		}
 
-    /**
-     * 判定两个对象的值是否相同
-     * @param {Any} a
-     * @param {Any} b
-     * @return {Boolean}
-     * @public
-     */
+		if (getType(O) == 'Array') {
+			while (k < len) {
+				if (hasOwn.call(O, k)) {
+					kValue = O[k];
+					if (callbackfn.call(kValue, k + 1, kValue, O) === false)
+						break;
+				}
+				k++;
+			}
+		} else {
+			for (k in O) {
+				if (hasOwn.call(O, k)) {
+					kValue = O[k];
+					if (callbackfn.call(kValue, kValue, k, O) === false)
+						break;
+				}
+			}
+		}
+		return undefined;
+	}
 
-    Util.isEqual=function(a, b) {
-        if(a === b) {
-            return true;
-        } else if(a === null || b === null || typeof a === "undefined" || typeof b === "undefined" || $.type(a) !== $.type(b)) {
-            return false;
+	/**
+	 * 判定两个对象的值是否相同
+	 * @param {Any} a
+	 * @param {Any} b
+	 * @return {Boolean}
+	 * @public
+	 */
+
+	Util.isEqual = function (a, b) {
+		if (a === b) {
+			return true;
+		} else if (a === null || b === null || typeof a === "undefined" || typeof b === "undefined" || $.type(a) !== $.type(b)) {
+			return false;
+		} else {
+			switch ($.type(a)) {
+			case "String":
+			case "Boolean":
+			case "Number":
+			case "Null":
+			case "Undefined":
+				//处理简单类型的伪对象与字面值相比较的情况,如1 v new Number(1)
+				if (b instanceof a.constructor || a instanceof b.constructor) {
+					return a == b;
+				}
+				return a === b;
+			case "NaN":
+				return isNaN(b);
+			case "Date":
+				return +a === +b;
+			case "NodeList":
+			case "Arguments":
+			case "Array":
+				var len = a.length;
+				if (len !== b.length)
+					return false;
+				for (var i = 0; i < len; i++) {
+					if (!isEqual(a[i], b[i])) {
+						return false;
+					}
+				}
+				return true;
+			default:
+				for (var key in b) {
+					if (!isEqual(a[key], b[key])) {
+						return false;
+					}
+				}
+				return true;
+			}
+		}
+	}
+	//from jquery
+	Util.isWindow = function (win) {
+		return (win && typeof win === "object" && "setInterval" in win);
+	}
+	Util.isEmpty =function (object) {
+        if (Util.getType(object)=='Array') {
+            return object.length == 0;
         } else {
-            switch($.type(a)) {
-                case "String":
-                case "Boolean":
-                case "Number":
-                case "Null":
-                case "Undefined":
-                    //处理简单类型的伪对象与字面值相比较的情况,如1 v new Number(1)
-                    if(b instanceof a.constructor || a instanceof b.constructor) {
-                        return a == b;
-                    }
-                    return a === b;
-                case "NaN":
-                    return isNaN(b);
-                case "Date":
-                    return +a === +b;
-                case "NodeList":
-                case "Arguments":
-                case "Array":
-                    var len = a.length;
-                    if(len !== b.length) return false;
-                    for(var i = 0; i < len; i++) {
-                        if(!isEqual(a[i], b[i])) {
-                            return false;
-                        }
-                    }
-                    return true;
-                default:
-                    for(var key in b) {
-                        if(!isEqual(a[key], b[key])) {
-                            return false;
-                        }
-                    }
-                    return true;
+            for (var k in object) {
+                return false;
             }
+            return true;
         }
     }
-	
+
 	return Util
 }
-
-)
- 
+	())
