6b895a0bfdf8380a968a2d2bdf2489a5c75ca697
jty <jtyjty99999@126.com>
2013-09-27 17:52:49 +0800

    fix bug

--- src/more/router.js
+++ src/more/router.js
@@ -1,4 +1,4 @@
-/**
+	/**
  * @module Router
  *
  * location hash驱动的路由器
@@ -21,16 +21,16 @@
 
 	var queryStringReg = /\?([^#]*)?$/; //?abcdef  查询字符串
 	var hashStart = /^[^#]*#/; //aaaaaaaa#hash   hash值前的数据
-	var queryParam = /:([^\/]+)/g//#/test/:hello  用hello可以拿到#/test/abc  的abc ,也可以触发跳转
+	var queryParam = /:([^\/]+)/g; //#/test/:hello  用hello可以拿到#/test/abc  的abc ,也可以触发跳转
 
 
-		//增加哈希请求,用户输入一个类似www.baidu.com/123#hello?jsdk
-		//拿到的哈希值为hello
-		//用户也可以使用自定义的router名字
-		//用户同样可以给router传参数.
-		//www.baidu.com/get#/test/a
-		//当hash切换为 www.baidu.com/get#/test/bbbb 也触发
-		Router.on = function (name, path, handler) {
+	//增加哈希请求,用户输入一个类似www.baidu.com/123#hello?jsdk
+	//拿到的哈希值为hello
+	//用户也可以使用自定义的router名字
+	//用户同样可以给router传参数.
+	//www.baidu.com/get#/test/a
+	//当hash切换为 www.baidu.com/get#/test/bbbb 也触发
+	Router.on = function (name, path, handler) {
 		if (arguments.length === 2) {
 			handler = path;
 			path = name;
@@ -46,22 +46,22 @@
 			flag : flag
 		};
 		console.log(routerPackage)
-	};
-	Router.handleRegisterPath(path) {
-
-		return path
-
 	}
 
-	Router.handleRoutePath(path) {
+	Router.handleRegisterPath= function(path) {
+		var path = path;
+		return path
+	}
+
+	Router.handleRoutePath= function(path) {
 		console.log('beforeParsing' + path)
 		path = path.replace(queryStringReg, '').replace(hashStart, '');
-		console.log('afterParsing' + path)
+		console.log('afterParsing' + path);
+		
 		return {
 			path : path,
 			param : 222
 		}
-
 	}
 	Router.routeName = function (name) {
 		var flag = routerNames[name];
@@ -69,17 +69,16 @@
 	}
 
 	Router.run = function (router) {
-		router[handler].call(null, parsed.param);
+		router['handler'].call(null, parsed.param);
 	}
 
 	Router.routePath = function (path) {
 		var parsed = Router.handleRoutePath(path);
-		console.log(parsed)
 		var l = routerPackage.length,
-		i = 0;
+		i = 1;
 		for (; i < l; i++) {
-			if (routerPackage[i][hasher] == parsed.path) {
-				routerPackage[i][handler].call(null, parsed.param)
+			if (routerPackage[i]['hasher'] == parsed.path) {
+				routerPackage[i]['handler'].call(null, parsed.param)
 				break
 			}
 
