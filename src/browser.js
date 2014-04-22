7a4f7d9006c25f4352b3cf196b722b273f556e80
Adams <jtyjty99999@126.com>
2014-04-22 11:35:38 +0800

    Update browser.js
    add css3 detector

--- src/browser.js
+++ src/browser.js
@@ -606,7 +606,35 @@
 		}
 	}
 
-	
+	/**
+	 * 判断浏览器是否支持某一个CSS3属性
+	 * @param  {String} 属性名称
+	 * @return {Boolean} true/false
+	 */
+	 
+	Browser.supportCss3(style) {
+		var prefix = ['webkit', 'Moz', 'ms', 'o'],
+			i,
+			humpString = [],
+			htmlStyle = document.documentElement.style,
+			_toHumb = function (string) {
+				return string.replace(/-(\w)/g, function ($0, $1) {
+					return $1.toUpperCase();
+				});
+			};
+	 
+		for (i in prefix){
+			humpString.push(_toHumb(prefix[i] + '-' + style));
+		}
+	 
+		humpString.push(_toHumb(style));
+	 
+		for (i in humpString){
+			if (humpString[i] in htmlStyle) return true;
+		}
+	 
+		return false;
+	}
 	
 	
 	return Browser
