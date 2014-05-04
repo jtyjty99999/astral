34f608801fc5b09f7de1c8002491ce600e8a7652
Adams <jtyjty99999@126.com>
2014-05-04 15:08:25 +0800

    Update touchtool.js

--- src/mobile/touchtool.js
+++ src/mobile/touchtool.js
@@ -12,6 +12,23 @@ function isPrimaryTouch(event) {
 	}
 }
 //http://quojs.tapquo.com/ http://code.baidu.com/
+//for more use
+function calc(x1, y1, x2, y2, x3, y3, x4, y4) {
+    var rotate = Math.atan2(y4 - y3, x4 - x3) - Math.atan2(y2 - y1, x2 - x1),
+        scale = Math.sqrt((Math.pow(y4 - y3, 2) + Math.pow(x4 - x3, 2)) / (Math.pow(y2 - y1, 2) + Math.pow(x2 - x1, 2))),
+        translate = [x3 - scale * x1 * Math.cos(rotate) + scale * y1 * Math.sin(rotate), y3 - scale * y1 * Math.cos(rotate) - scale * x1 * Math.sin(rotate)]
+        ;
+    return {
+        rotate: rotate,
+        scale: scale,
+        translate: translate,
+        matrix: [
+            [scale * Math.cos(rotate), -scale * Math.sin(rotate), translate[0]],
+            [scale * Math.sin(rotate), scale * Math.cos(rotate), translate[1]],
+            [0, 0, 1]
+        ]
+    }
+}
 
 
 var touchEvent = isWP ? {
