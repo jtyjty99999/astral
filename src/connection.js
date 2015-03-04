c35d71f4018339964b7af4a101fb8c6cdb4bd747
Adams <jtyjty99999@126.com>
2015-03-04 23:25:10 +0800

    Update connection.js

--- src/connection.js
+++ src/connection.js
@@ -565,7 +565,38 @@
 		xml.send();
 
 	}
+	//网络状态判断
 	
+	C.netWork = function(){
+	    var wifi = true;
+	    var ua = windows.navigator.userAgent;
+	    var con = windows.navigator.connection;
+	    // 如果是微信
+	    if(/MicroMessenger/.test(ua)){
+	        // 如果是微信6.0以上版本，用UA来判断
+	        if(/NetType/.test(ua)){
+	            if(ua.match(/NetType\/(\S*)$/)[1] != 'WIFI'){
+	                wifi = false;
+	            }
+	        // 如果是微信6.0以下版本，调用微信私有接口WeixinJSBridge
+	        }else{
+	            document.addEventListener("WeixinJSBridgeReady",function onBridgeReady(){
+	                WeixinJSBridge.invoke('getNetworkType',{},function(e){
+	                    if(e.err_msg != "network_type:wifi"){
+	                        wifi = false;
+	                    }
+	                });
+	            });
+	        }
+	    // 如果支持navigator.connection
+	    }else if(con){
+	        var network = con.type;
+	        if(network != "wifi" && network != "2" && network != "unknown"){  // unknown是为了兼容Chrome Canary
+	            wifi = false;
+	        }
+	    }
+	    return wifi
+	};	
 	
 	C.fastImgReady = (function () {
 		var list = [], intervalId = null,
